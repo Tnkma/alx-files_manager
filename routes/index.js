01e203606f273ecfa7484593884a701010a97fc2
Godswill <onwusilikegodswill@gmail.com>
2024-07-03 00:29:17 +0100

    express app

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,11 @@
-GET /status => AppController.getStatus
-GET /stats => AppController.getStats
+import  { Router } from 'express';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+// Define the routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+// Export the router
+export default router;
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+
+// initialize express just like we do for flask
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Use routes defined in routes/index.js
+app.use('/', routes);
+
+// Were are we going to be listening for requests
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+// Export the app
+export default app;
